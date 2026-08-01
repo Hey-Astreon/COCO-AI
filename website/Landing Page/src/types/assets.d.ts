da636b765702f0696d3584b396f5f9e915fcfc83
Roushan <playboxstation460@gmail.com>
2026-08-01 21:21:30 +0530

    fix: resolve asset type declarations in assets.d.ts and include declaration files in tsconfig.json

--- website/Landing Page/src/types/assets.d.ts
+++ website/Landing Page/src/types/assets.d.ts
@@ -12,3 +12,48 @@ declare module "*.asset.json" {
   };
   export default value;
 }
+
+declare module "*.css" {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module "*.scss" {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module "*.webp" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.png" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.jpg" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.jpeg" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.svg" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.ico" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.gif" {
+  const src: string;
+  export default src;
+}
